81c2ce63c49a206516108060dd0a0e237aaa1e7c
Palcoin <adekunleoresanya@yahoo.com>
2016-05-17 20:24:23 +0200

    Initial Commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define palcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "palcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
